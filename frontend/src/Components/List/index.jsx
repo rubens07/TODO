18fba3c7bba5085e7af64310a003dd151e141f4a
rubens <rubens@gpr.com.br>
2022-02-13 10:02:00 -0300

    Criado componente para cada tarefa junto com CSS

--- frontend/src/Components/List/index.jsx
+++ frontend/src/Components/List/index.jsx
@@ -1,13 +1,13 @@
 import Item from './item';
 
-const List = ({ title, listItens }) => {
+const List = ({ title, listItens, todo }) => {
   return (
     <div className="group">
       <h3>{title}</h3>
       <hr />
-      <div className="list">
-        {listItens.map((task, num) => (
-        <Item text={task.description} key={num} />
+      <div>
+        {listItens.map(task => (
+        <Item text={task.description} key={task.id} todo={todo} />
         ))}
       </div>
     </div>
--- frontend/src/Components/List/item.jsx
+++ frontend/src/Components/List/item.jsx
@@ -1,6 +1,60 @@
-const Item = ({ text }) => {
+import React, { useState } from 'react';
+
+const Item = ({ text, todo }) => {
+  const [edit, setEdit] = useState(false);
+  const [task, setTask] = useState(text);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setTask(event.target.value);
+  };
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  }
+
+  // Botão Edit: aparece apenas na lista TODO
+  // Botão Check: aparece apenas na lista TODO
+  // Botão Excluir: aparece apenas na lista Done
+  // Botão Uncheck: Aparece apenas na lista Done
   return (
-    <div>{text}</div>
+    <div className="item">
+      <div className='item-text'>
+        {edit && (
+          <input
+            type="text"
+            name="editForm"
+            autoFocus value={task}
+            onChange={handleChange}
+          />
+        )}
+        {!edit && (
+          <span className={todo ? '' : 'done'}>{task}</span>
+        )}
+      </div>
+      <div className='item-buttons'>
+        {todo && (
+          <>
+            <button onClick={handleEdit} >
+              <img src='edit.ico' alt='Edit Task' />
+            </button>
+            <button onClick={handleEdit} >
+              <img src='check1.ico' alt='Edit Task' />
+            </button>
+          </>
+        )}
+        {!todo && (
+          <>
+            <button onClick={handleEdit} >
+              <img src='delete.ico' alt='Edit Task' />
+            </button>
+            <button onClick={handleEdit} >
+              <img src='back.ico' alt='Edit Task' />
+            </button>
+          </>
+        )}
+      </div>
+    </div>
   )
 };
 
